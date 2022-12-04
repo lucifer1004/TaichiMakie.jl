85a6054223a45a06cee76f6dfad5f04fa4a43e7b
Gabriel Wu <wuzihua@pku.edu.cn>
2022-12-04 10:21:24 +0800

    use visible.jl for sysimage creation

--- deps/create_sysimage.jl
+++ deps/create_sysimage.jl
@@ -7,4 +7,4 @@ using PackageCompiler
 
 create_sysimage(["TaichiMakie"], sysimage_path = "taichi_makie.so",
                 precompile_execution_file = joinpath((@__DIR__, "..", "examples",
-                                                      "scatter.jl")))
+                                                      "visible.jl")))
